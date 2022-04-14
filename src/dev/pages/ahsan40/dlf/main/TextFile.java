5d934e46af663db91a921feb8017f46788ff3614
Ahsan Khan <nobody.ahsan@gmail.com>
2022-04-14 13:10:52 +0600

    [UPDATED] TextFile class

--- src/dev/pages/ahsan40/dlf/main/TextFile.java
+++ src/dev/pages/ahsan40/dlf/main/TextFile.java
@@ -3,15 +3,47 @@ package dev.pages.ahsan40.dlf.main;
 import java.io.File;
 
 public class TextFile {
-    boolean ignoreEmptyLines;
-    boolean ignoreWhiteSpace;
-    boolean caseSensitive;
-    File file;
+    private boolean ignoreEmptyLines;
+    private boolean ignoreWhiteSpace;
+    private boolean caseSensitive;
+    private File file;
 
     public TextFile(boolean ignoreEmptyLines, boolean ignoreWhiteSpace, boolean caseSensitive, File file) {
         this.ignoreEmptyLines = ignoreEmptyLines;
         this.ignoreWhiteSpace = ignoreWhiteSpace;
         this.caseSensitive = caseSensitive;
+        this.file = file;
+    }
+
+    public boolean isIgnoreEmptyLines() {
+        return ignoreEmptyLines;
+    }
+
+    public void setIgnoreEmptyLines(boolean ignoreEmptyLines) {
+        this.ignoreEmptyLines = ignoreEmptyLines;
+    }
+
+    public boolean isIgnoreWhiteSpace() {
+        return ignoreWhiteSpace;
+    }
+
+    public void setIgnoreWhiteSpace(boolean ignoreWhiteSpace) {
+        this.ignoreWhiteSpace = ignoreWhiteSpace;
+    }
+
+    public boolean isCaseSensitive() {
+        return caseSensitive;
+    }
+
+    public void setCaseSensitive(boolean caseSensitive) {
+        this.caseSensitive = caseSensitive;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
         this.file = file;
     }
 }
