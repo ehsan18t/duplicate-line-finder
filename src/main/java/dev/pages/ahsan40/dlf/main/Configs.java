e8a5db10322bb4ad6497e67d9453052dbeba0d71
Ahsan Khan <mkhan201122@bscse.uiu.ac.bd>
2022-09-29 15:16:03 +0600

    migrated to maven

--- src/main/java/dev/pages/ahsan40/dlf/main/Configs.java
+++ src/main/java/dev/pages/ahsan40/dlf/main/Configs.java
@@ -2,16 +2,16 @@ package dev.pages.ahsan40.dlf.main;
 
 public class Configs {
     // Pages
-    public static final String homePage = "/resources/fxml/Home.fxml";
-    public static final String resultPage = "/resources/fxml/Result.fxml";
+    public static final String homePage = "/fxml/Home.fxml";
+    public static final String resultPage = "/fxml/Result.fxml";
     // Author Info
     public static final String author = "Ahsan";
     public static final String gitProfile = "https://github.com/Ahsan40";
     public static final String gitRepo = gitProfile + "/duplicate-line-finder";
     // Resources
-    public static String css = "/resources/css/light.css";
+    public static String css = "/css/light.css";
     // App Info
     public static String title = "Duplicate Line Finder";
     public static String version = "v1.0";
-    public static String icon = "/resources/img/icon.png";
+    public static String icon = "/img/icon.png";
 }
